c7a2775a0c653edb65f08c9362fa577e1bcfda5d
Wilson Gomes <contato@wilsonneto.com.br>
2019-12-03 08:47:27 -0300

    redux na primeira tela, fixed costs

---
+++ src/store/ducks/fixedCosts.js
@@ -0,0 +1,35 @@
+export const Types = {
+  ADD: "todos/ADD_FIXED_COST",
+  DEL: "todos/DEL_FIXED_COST"
+};
+
+const INITIAL_STATE_FIXED_COST = [
+  { id: 1, description: "Aluguel", cost: 1500 },
+  { id: 2, description: "Salários", cost: 1000 },
+  { id: 3, description: "Encargos Sociais", cost: 400 }
+];
+
+export default function fixedCosts(state = INITIAL_STATE_FIXED_COST, action) {
+  console.log("state", state);
+
+  switch (action.type) {
+    case Types.ADD:
+      return [...state, action.payload];
+    case Types.DEL:
+      return [...state.filter(item => item.id !== action.payload.id)];
+    default:
+      return state;
+  }
+}
+
+export const Creators = {
+  addFixedCost: payload => ({
+    type: Types.ADD,
+    payload
+  }),
+
+  delFixedCost: id => ({
+    type: Types.DEL,
+    payload: { id }
+  })
+};
---
+++ src/store/ducks/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from "redux";
+
+import fixedCosts from "./fixedCosts";
+
+export default combineReducers({ fixedCosts });
